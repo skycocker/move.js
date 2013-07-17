36d8e875adfc42512c042583759511d1abf59463
Michał Siwek <mike21@aol.pl>
2013-07-17 19:42:38 +0100

    Rearrange the code, make it easier to use
    Wraps the code to an anonymous self-executing function, creates global movejs object with simple init() method

--- move.js
+++ move.js
@@ -1,195 +1,187 @@
-window.requestAnimationFrame = window.requestAnimationFrame ||
-                               window.msRequestAnimationFrame ||
-                               window.mozRequestAnimationFrame ||
-                               window.webkitRequestAnimationFrame;
-
-navigator.getMedia = navigator.getUserMedia ||
-                     navigator.webkitGetUserMedia ||
-                     navigator.mozGetUserMedia ||
-                     navigator.msGetUserMedia;
-
-var video = document.createElement("video");
-video.width = 640;
-video.height = 480;
-video.style.display = "none";
-document.body.appendChild(video);
-
-var width = video.width,
-    height = video.height;
-
-var staticDump = document.createElement("canvas");
-    staticDump.width = width;
-    staticDump.height = height;
-var staticCtx = staticDump.getContext("2d");
-    //vertical flip
-    staticCtx.translate(width, 0);
-    staticCtx.scale(-1, 1);
-
-var output = document.querySelector("#movejs-workspace"),
-    ctx = output.getContext("2d");
-    //vertical flip
-    ctx.translate(width, 0);
-    ctx.scale(-1, 1);
-
-var buffer = new Uint8Array(width * height);
-
-var isLittleEndian = null;
-
-var valueR = 0,
-    valueG = 170,
-    valueB = 242;
-
-//readyToMove event
-var readyToMove = new CustomEvent(
-  "readyToMove", {
-    bubbles: true,
-    cancelable: true
-  }
-);
-
-function initMoves() {
-  navigator.getMedia({ video: true }, startStream, logError);
-}
-
-function startStream(stream) {
-  video.src = URL.createObjectURL(stream);
-  video.play();
+(function(window, document, navigator) {
+  window.requestAnimationFrame = window.requestAnimationFrame ||
+                                 window.msRequestAnimationFrame ||
+                                 window.mozRequestAnimationFrame ||
+                                 window.webkitRequestAnimationFrame;
   
-  requestAnimationFrame(drawStatic);
-}
+  navigator.getUserMedia = navigator.getUserMedia ||
+                           navigator.webkitGetUserMedia ||
+                           navigator.mozGetUserMedia ||
+                           navigator.msGetUserMedia;
 
-function logError(error) {
-  console.log("error: " + error);
-}
-
-function drawStatic() {
-  setInterval(function() {
-    document.querySelector(".movejs-cameraAdjustIn").value += 100 / 8;
-  }, 500);
+  var video = document.createElement("video");
   
-  setTimeout(function() {
-    var staticFrame = readFrame(staticCtx);
-    
-    if(staticFrame) {
-      staticCtx.putImageData(staticFrame, 0, 0);
-      setupBuffer(staticFrame.data);
+  var staticDump = document.createElement("canvas"),
+      staticCtx = staticDump.getContext("2d");
+
+  var output = null,
+      ctx = null;
+
+  var buffer = null;
+  var isLittleEndian = null;
+
+  var readyToMove = new CustomEvent(
+    "readyToMove", {
+      bubbles: true,
+      cancelable: true
+    }
+  );
+
+  function Move() {
+    this.init = function(output, width, height, humanFill, step, limitX, limitY) {
+      this.width = width || 640;
+      this.height = height || 480;
+      this.humanFill = humanFill || { r: 0, g: 170, b: 242 };
+      this.step = step;
+      this.limitX = this.width;
+      this.limitY = this.height;
       
-      setTimeout(function() {
-        document.dispatchEvent(readyToMove);
-        requestAnimationFrame(draw);
-      }, 1000);
-    } else {
-      requestAnimationFrame(drawStatic);
-    }
-  }, 3000);
-}
+      var outputId = output || "move";
+      output = document.querySelector("#" + outputId),
+      ctx = output.getContext("2d");
+      
+      video.width = staticDump.width = output.width = this.width;
+      video.height = staticDump.height = output.height = this.height;
 
-function draw() {
-  var frame = readFrame(ctx);
-  
-  if(frame) {
-    markLightnessChanges(frame.data);
-    ctx.putImageData(frame, 0, 0);
-    
-    //flip for a second...
-    ctx.translate(width, 0);
-    ctx.scale(-1, 1);
-    //draw game frame
-    game.step();
-    //...aaand flip back
-    ctx.translate(width, 0);
-    ctx.scale(-1, 1);
-  }
+      //vertical flip
+      staticCtx.translate(this.width, 0);
+      staticCtx.scale(-1, 1);
 
-  requestAnimationFrame(draw);
-}
+      ctx.translate(this.width, 0);
+      ctx.scale(-1, 1);
+      //
 
-function markAreas(onto) {
-  onto.fillStyle = "rgba(0, 209, 56, 0.2)";
-  onto.fillRect(160, 0, 330, 480);
-}
+      buffer = new Uint8Array(this.width * this.height);
 
-function readFrame(onto) {
-  try {
-    onto.drawImage(video, 0, 0, width, height);
-  } catch(e) {
-    return null;
-  }
-  
-  return onto.getImageData(0, 0, width, height);
-}
-
-function setupBuffer(data) {
-  for(var i=0, j=0; i<buffer.length; i++, j+=4) {
-    buffer[i] = lightnessValue(data[j], data[j+1], data[j+2]);
-  }
-}
-
-function markLightnessChanges(data) {
-  var buf = new ArrayBuffer(data.length);
-  var buf8 = new Uint8ClampedArray(buf);
-  var outData = new Uint32Array(buf);
-  
-  if(isLittleEndian == null) {
-    outData[1] = 0x0a0b0c0d;
-    if(buf[4] === 0x0a && buf[5] === 0x0b && buf[6] === 0x0c && buf[7] === 0x0d) {
-      isLittleEndian = false;
-    } else {
-      isLittleEndian = true;
+      navigator.getUserMedia({ video: true }, startStream, logError);
     }
   }
-  
-  var limitY = height;
-  if(!drawAll) {
-    var limitX = 150;
-  } else {
-    var limitX = width;
+
+  var move = window.movejs = new Move();
+
+  function startStream(stream) {
+    video.src = URL.createObjectURL(stream);
+    video.play();
+
+    requestAnimationFrame(drawStatic);
   }
-  
-  if(isLittleEndian) {
-    for(var y=0; y<limitY; ++y) {
-      for(var x=0; x<limitX; ++x) {
-        var rawOffset = y * width + x;
-        var index = rawOffset * 4;
-        
-        var current = lightnessValue(data[index], data[index+1], data[index+2]);
-        
-        outData[rawOffset] = 
-            (255 * lightnessHasChanged(rawOffset, current) << 24) | //a
-            (valueB << 16) |                                        //b
-            (valueG << 8) |                                         //g
-            valueR;
+
+  function logError(error) {
+    console.log("error: " + error);
+  }
+
+  function drawStatic() {
+    setTimeout(function() {
+      var staticFrame = readFrame(staticCtx);
+
+      if(staticFrame) {
+        staticCtx.putImageData(staticFrame, 0, 0);
+        setupBuffer(staticFrame.data);
+
+        setTimeout(function() {
+          document.dispatchEvent(readyToMove);
+          requestAnimationFrame(draw);
+        }, 1000);
+      } else {
+        requestAnimationFrame(drawStatic);
+      }
+    }, 3000);
+  }
+
+  function draw() {
+    var frame = readFrame(ctx);
+
+    if(frame) {
+      markLightnessChanges(frame.data);
+      ctx.putImageData(frame, 0, 0);
+
+      //flip for a second...
+      ctx.translate(move.width, 0);
+      ctx.scale(-1, 1);
+      //execute external callback
+      if(move.step) move.step();
+      //...aaand flip back
+      ctx.translate(move.width, 0);
+      ctx.scale(-1, 1);
+    }
+
+    requestAnimationFrame(draw);
+  }
+
+  function readFrame(onto) {
+    try {
+      onto.drawImage(video, 0, 0, move.width, move.height);
+    } catch(e) {
+      return null;
+    }
+
+    return onto.getImageData(0, 0, move.width, move.height);
+  }
+
+  function setupBuffer(data) {
+    for(var i=0, j=0; i<buffer.length; i++, j+=4) {
+      buffer[i] = lightnessValue(data[j], data[j+1], data[j+2]);
+    }
+  }
+
+  function markLightnessChanges(data) {
+    var buf = new ArrayBuffer(data.length);
+    var buf8 = new Uint8ClampedArray(buf);
+    var outData = new Uint32Array(buf);
+
+    if(isLittleEndian == null) {
+      outData[1] = 0x0a0b0c0d;
+      if(buf[4] === 0x0a && buf[5] === 0x0b && buf[6] === 0x0c && buf[7] === 0x0d) {
+        isLittleEndian = false;
+      } else {
+        isLittleEndian = true;
       }
     }
-  } else {
-    for(var y=0; y<limitY; ++y) {
-      for(var x=0; x<limitX; ++x) {
-        var rawOffset = y * width + x;
-        var index = rawOffset * 4;
-        
-        var current = lightnessValue(data[index], data[index+1], data[index+2]);
-        
-        outData[rawOffset] = 
-            (0 << 24) |                                     //r
-            (170 << 16) |                                   //g
-            (242 << 8) |                                    //b
-            255 * lightnessHasChanged(rawOffset, current);  //a
+
+    if(isLittleEndian) {
+      for(var y=0; y<move.limitY; ++y) {
+        for(var x=0; x<move.limitX; ++x) {
+          var rawOffset = y * move.width + x;
+          var index = rawOffset * 4;
+
+          var current = lightnessValue(data[index], data[index+1], data[index+2]);
+
+          outData[rawOffset] =
+              (255 * lightnessHasChanged(rawOffset, current) << 24) | //a
+              (move.humanFill.b << 16) |                              //b
+              (move.humanFill.g << 8) |                               //g
+              move.humanFill.r;                                       //r
+        }
+      }
+    } else {
+      for(var y=0; y<move.limitY; ++y) {
+        for(var x=0; x<move.limitX; ++x) {
+          var rawOffset = y * move.width + x;
+          var index = rawOffset * 4;
+
+          var current = lightnessValue(data[index], data[index+1], data[index+2]);
+
+          outData[rawOffset] =
+              (move.humanFill.r << 24) |                     //r
+              (move.humanFill.g << 16) |                     //g
+              (move.humanFill.b << 8) |                      //b
+              255 * lightnessHasChanged(rawOffset, current); //a
+        }
       }
     }
+
+    data.set(buf8);
   }
-  
-  
-  data.set(buf8);
-}
 
-function lightnessHasChanged(index, value) {
-  return qAbs(value - buffer[index]) >= 15;
-}
+  function lightnessHasChanged(index, value) {
+    return qAbs(value - buffer[index]) >= 15;
+  }
 
-function lightnessValue(r, g, b) {
-  return ( Math.min(r, g, b) + Math.max(r, g, b) ) / 255 * 50;
-}
+  function lightnessValue(r, g, b) {
+    return ( Math.min(r, g, b) + Math.max(r, g, b) ) / 255 * 50;
+  }
 
-function qAbs(number) {
-  return (number ^ (number >> 31)) - (number >> 31);
-}
+  function qAbs(number) {
+    return (number ^ (number >> 31)) - (number >> 31);
+  }
+})(window, document, navigator)
